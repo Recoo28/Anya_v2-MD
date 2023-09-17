71fc9e3e03c203475264f78f6b5fca48e945c03f
Recoo28 <61563638+Recoo28@users.noreply.github.com>
2023-09-17 14:57:26 +0300

    Update config.js

--- config.js
+++ config.js
@@ -22,26 +22,26 @@ global.lApiKey = {// if this API key get expired then please go to https://api.l
 }
 
 
-global.botname = "Queen Anya" 
-global.author = "Enter Author Name Here"  
-global.packname = "𝙌𝙐𝙀𝙀𝙉✯𝘼𝙉𝙔𝘼✯𝘽𝙊𝙏"  
-global.myweb = "Enter Your Any Website Profile Link Here"
+global.botname = "Fakedosty" 
+global.author = "Fakedosty"  
+global.packname = ""  
+global.myweb = "https://linktr.ee/fakedosty_one_cikan"
 global.footer = "©\t" + "Enter Your Watermark Here"
-global.prefa = ['-'] // single prefix
+global.prefa = [','] // single prefix
 global.themeemoji = "🎐"
 global.autoBio = "$mode : $time ⌚ || $date 🗓️ || $botname" // Leave it as it is if you didn't understood!
 global.socialText = `*⪧⪢ Please join this group for by bot's announcement.*`
 global.socialLink = `*⪧⪢ https://chat.whatsapp.com/HshHS6kHF9NHnA9lfMwwSM 💝*`
 
 
-global.ownername = "Enter Owner Name Here"    
-global.ownernumber = ["918811074852"]
+global.ownername = "Reco"    
+global.ownernumber = ["905436985383"]
 global.ytname = "YT: Enter Your Channel Name Here"
 global.continent = "Asia" // your continent name 
-global.region = "Enter Your Country Name Here"
+global.region = "Turkey"
 global.state = "Enter Your State Name Here" // your state name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "Enter Your Insta ID Here"
+global.timezone = "Asia/colombo" // search on google if you don't know the timezone of your country //
+global.instagramId = "Fakedosty"
 global.email = "Enter Your Email ID Here"
 
 
